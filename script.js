3913fb516dfbf066b687110f3b97ee299bf4c5b7
MarceloKobayashi <marcelohk2004@gmail.com>
2024-08-13 10:34:36 -0300

    Implementando validação no script

---
+++ script.js
@@ -0,0 +1,32 @@
+let titulo = document.getElementById("titulo").value;
+console.log(titulo);
+
+function submeter() {
+    let nome = document.getElementById("nome").value;
+    let idade = document.getElementById("idade").value;
+    let cpf = document.getElementById("cpf").value;
+
+    console.log(cpf)
+    console.log(validaCPF(cpf))
+}
+
+function validaCPF(cpf) {
+    cpf = cpf.trim()
+
+    if (cpf == "") {
+        alert("Campo CPF não pode ser vazio.");
+        return false;
+    }
+
+    if (/[a-zA-Z]/.test(cpf)) {
+        alert("CPF não pode conter letras.");
+        return false;
+    }
+
+    if (!/^[\d.-]+$/.test(cpf)) {    //Do início ao fim, só tem [\d.-] uma ou mais vezes, true
+        alert("CPF só pode conter números, '.' ou '-'");
+        return false;
+    }
+
+    return true;
+}
