6817b977bdfc6a1b882143baee21aa71a56ad369
MarceloKobayashi <marcelohk2004@gmail.com>
2024-08-13 17:07:08 -0300

    Validaçao do cpf concluida.

--- script.js
+++ script.js
@@ -6,8 +6,14 @@ function submeter() {
     let idade = document.getElementById("idade").value;
     let cpf = document.getElementById("cpf").value;
 
-    console.log(cpf)
-    console.log(validaCPF(cpf))
+    console.log(cpf);
+    
+    const isValidCPF = validaCPF(cpf);
+    console.log(isValidCPF);
+
+    if (isValidCPF) {
+        console.log(calculoCPF(cpf));
+    }
 }
 
 function validaCPF(cpf) {
@@ -29,18 +35,64 @@ function validaCPF(cpf) {
         return false;
     }
 
-    if (length(cpf) != 11 || length(cpf) != 14) {
+    if (cpf.length != 11 && cpf.length != 14) {
         alert("CPF deve ter 11 ou 14(com pontos e hífen) dígitos.");
         return false;
     }
 
-    if (length(cpf) == 11 && !/^[\d]+$/) {
+    if (cpf.length == 11 && !/^\d{11}$/.test(cpf)) {
         //calculo cpf valido
         alert("Reescreva o CPF com apenas números ou no formato 123.123.123-12");
         return false;
     }
 
-    if (length(cpf) == 14 && !/[\d].[\d].[\d]-[\d]/)
+    if (cpf.length == 14 && !/^\d{3}\.\d{3}\.\d{3}-\d{2}$/.test(cpf)) {
+        alert("Reescreva o CPF com apenas números ou no formato 321.321.321-32");
+        return false;
+    }
+
+    return true;
+}
+
+function calculoCPF(cpf) {
+    cpf = cpf.replace(".", "");
+    cpf = cpf.replace("-", "");
+
+    let primeiraSoma = 0;
+    let primeiroElemento = 10;
+
+    for (let i = 0; i < 9; i++) {
+        let numero = Number(cpf[i]);
+        primeiraSoma += numero * primeiroElemento;
+        primeiroElemento--;
+    }
+
+    let primeiroDigito = 11 - (primeiraSoma % 11);
+
+    if (primeiroDigito >= 10) {primeiroDigito = 0;}
+
+    if (primeiroDigito != cpf[9]) {
+        alert("Primeiro dígito verificador inválido, insira um CPF válido.");
+        return false;
+    }
+
+    let segundaSoma = 0;
+    let segundoElemento = 11;
+
+    for (let i = 0; i < 10; i++) {
+        let numero = Number(cpf[i]);
+        segundaSoma += numero * segundoElemento;
+        segundoElemento--;
+    }
+
+    let segundoDigito = 11 - (segundaSoma % 11);
+
+    if (segundoDigito >= 10) {segundoDigito = 0;}
+
+    if (segundoDigito != cpf[10]) {
+        alert("Segundo dígito verificador inválido, insira um CPF válido.");
+        return false;
+    }
 
     return true;
 }
